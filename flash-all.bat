7bad3ffc3ae5585c6efd20d757b7754de0f37f01
Joy <29514446+fillwithjoy1@users.noreply.github.com>
2024-06-28 11:51:27 +0000

    Update flash-all scripts

--- flash-all.bat
+++ flash-all.bat
@@ -7,6 +7,7 @@ fastboot flash vendor_boot vendor_boot.img
 fastboot reboot fastboot
 timeout 5
 fastboot flash vendor_dlkm vendor_dlkm.img
+fastboot reboot
 
 echo Kernel flashed :)
 exit
--- flash-all.sh
+++ flash-all.sh
@@ -1,2 +1,11 @@
 #!/bin/sh
 
+fastboot flash boot boot.img
+fastboot flash dtbo dtbo.img
+fastboot flash vendor_boot vendor_boot.img
+fastboot reboot fastboot
+sleep 5
+fastboot flash vendor_dlkm vendor_dlkm.img
+fastboot reboot
+
+echo "Kernel flashed :)"
