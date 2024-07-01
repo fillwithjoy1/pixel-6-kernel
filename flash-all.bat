1410c929777e9d3f4e4884716e4fe6f059651f72
joysi <29514446+fillwithjoy1@users.noreply.github.com>
2024-07-01 14:22:00 +1000

    Updated auto-flashing scripts

--- flash-all.bat
+++ flash-all.bat
@@ -5,9 +5,9 @@ fastboot flash boot boot.img
 fastboot flash dtbo dtbo.img
 fastboot flash vendor_boot vendor_boot.img
 fastboot reboot fastboot
-timeout 5
 fastboot flash vendor_dlkm vendor_dlkm.img
 fastboot reboot
 
 echo Kernel flashed :)
+timeout 5
 exit
--- flash-all.sh
+++ flash-all.sh
@@ -4,8 +4,8 @@ fastboot flash boot boot.img
 fastboot flash dtbo dtbo.img
 fastboot flash vendor_boot vendor_boot.img
 fastboot reboot fastboot
-sleep 5
 fastboot flash vendor_dlkm vendor_dlkm.img
 fastboot reboot
 
 echo "Kernel flashed :)"
+sleep 5
