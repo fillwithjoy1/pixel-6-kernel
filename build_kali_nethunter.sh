36f0d62edca766babf835083d13684e1eebc8735
fillwithjoy1 <29514446+fillwithjoy1@users.noreply.github.com.>
2025-02-17 13:04:41 +1000

    Revert "Update build_kali_nethunter.sh"
    This reverts commit 6c5adbc4d02dc6c4ecc5c3822faea9fda0969477.

--- build_kali_nethunter.sh
+++ build_kali_nethunter.sh
@@ -10,7 +10,7 @@
 mkdir "kali-nethunter-kernel"
 cd "kali-nethunter-kernel" || echo "Failed to change directory to kali-nethunter-kernel. Stopping" && exit 1
 
-repo init -u https://android.googlesource.com/kernel/manifest -b android-gs-raviole-5.10-android15-qpr1
+repo init -u https://android.googlesource.com/kernel/manifest -b android-gs-raviole-5.10-android14-qpr3
 repo sync -c --no-tags
 
 git clone "https://github.com/fillwithjoy1/Kali-defconfig-checker"
